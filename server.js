db77f3d3963e5d37a79c9a30be35795a37213611
Anil Kakumanu <47668931+kakumanuanil@users.noreply.github.com>
2019-02-21 16:12:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -116,9 +116,9 @@ app.all('/*', function(req, res) {
     res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(3000);
+app.listen(8080);
 
-console.log('Express listening on port 3000.');
+console.log('Express listening on port 8080.');
 
 //Open browser
 //Below statements not necessary
